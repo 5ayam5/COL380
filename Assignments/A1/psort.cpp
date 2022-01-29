9e0d0a9b66582843d6ef204b7b0a179a1d000103
5ayam5 <sayam.sethi2@gmail.com>
2022-01-29 21:14:40 +0530

    greatly reduced number of tasks and improved parallel execution

--- Assignments/A1/psort.cpp
+++ Assignments/A1/psort.cpp
@@ -3,13 +3,12 @@
 #include <iostream>
 #include <omp.h>
 #include <string.h>
-#include <string>
 
 void SequentialSort(uint32_t *data, uint32_t n) {
   if (n <= 1)
     return;
   uint32_t nl = n / 2, nr = (n + 1) / 2;
-  uint32_t left[nl], right[nr];
+  uint32_t *left = new uint32_t[nl], *right = new uint32_t[nr];
   memcpy(left, data, nl * sizeof(uint32_t));
   memcpy(right, data + nl, nr * sizeof(uint32_t));
   SequentialSort(left, nl);
@@ -25,12 +24,14 @@ void SequentialSort(uint32_t *data, uint32_t n) {
     memcpy(data + i, left + l, (n - i) * sizeof(uint32_t));
   if (r < nr)
     memcpy(data + i, right + r, (n - i) * sizeof(uint32_t));
+  delete[] left;
+  delete[] right;
 }
 
 uint32_t binary_search(uint32_t *data, uint32_t p, uint32_t val) {
   uint32_t l = 0, r = p;
   while (l < r) {
-    uint32_t mid = (l + r) >> 1;
+    uint32_t mid = l + ((r - l) >> 1);
     if (val <= data[(mid + 1) * p])
       r = mid;
     else
@@ -40,7 +41,7 @@ uint32_t binary_search(uint32_t *data, uint32_t p, uint32_t val) {
 }
 
 void print_arr(uint32_t *data, uint32_t n) {
-  for (uint32_t j = 0; j < n; j++)
+  for (uint64_t j = 0; j < n; j++)
     std::cout << data[j] << ' ';
   std::cout << '\n';
 }
@@ -51,73 +52,68 @@ void ParallelSort(uint32_t *data, uint32_t n, int p) {
     return;
   }
 
-  uint32_t r[p * p + 1], sz = n / p, offset = (n % p) * (sz + 1);
-
+  uint64_t sz = n / p;
+  uint32_t r[p * p + 1], offset = (n % p) * (sz + 1);
   for (uint32_t j = 0; j < n % p; j++) {
-#pragma omp task shared(p, sz, r, data)
-    {
-      uint32_t off1 = p * j, off2 = j * (sz + 1);
-      for (uint32_t idx = 0; idx < p; idx++)
-        r[off1 + idx] = data[off2 + idx];
-    }
+    uint32_t off1 = p * j, off2 = j * (sz + 1);
+    for (uint32_t idx = 0; idx < p; idx++)
+      r[off1 + idx] = data[off2 + idx];
   }
   for (uint32_t j = n % p; j < p; j++) {
-#pragma omp task shared(p, offset, sz, r, data)
-    {
-      uint32_t off1 = p * j, off2 = offset + (j - n % p) * sz;
-      for (uint32_t idx = 0; idx < p; idx++)
-        r[off1 + idx] = data[off2 + idx];
-    }
+    uint32_t off1 = p * j, off2 = offset + (j - n % p) * sz;
+    for (uint32_t idx = 0; idx < p; idx++)
+      r[off1 + idx] = data[off2 + idx];
   }
-#pragma omp taskwait
   SequentialSort(r, p * p);
   r[p * p] = UINT32_MAX;
 
-  uint32_t num_t = omp_get_num_threads(), b_size_t[p + 1][num_t], b_id[n];
-  for (uint32_t j = 0; j <= p; j++) {
-#pragma omp task shared(b_size_t, num_t)
-    memset(b_size_t[j], 0, (num_t) * sizeof(uint32_t));
-  }
-#pragma omp taskwait
-#pragma omp parallel for num_threads(num_t)
-  for (uint32_t j = 0; j < n; j++) {
+  uint32_t num_t = omp_get_num_threads(), b_size_t[num_t][p],
+           *b_id = new uint32_t[n];
+  for (uint32_t j = 0; j < num_t; j++)
+    memset(b_size_t[j], 0, p * sizeof(uint32_t));
+  sz = ((uint64_t)n + p - 1) / p;
+  for (uint32_t t = 0; t < p; t++) {
+#pragma omp task shared(p, r, data, b_id, b_size_t)
     {
-      b_id[j] = binary_search(r, p, data[j]);
-      b_size_t[b_id[j] + 1][omp_get_thread_num()]++;
+      uint32_t start = t * sz, end = t == p - 1 ? n : (t + 1) * sz;
+      for (uint64_t j = start; j < end; j++) {
+        b_id[j] = binary_search(r, p, data[j]);
+        b_size_t[omp_get_thread_num()][b_id[j]]++;
+      }
     }
   }
+#pragma omp taskwait
 
   uint32_t b_size[p + 1];
-  for (uint32_t j = 0; j <= p; j++) {
+  b_size[0] = 0;
+  for (uint32_t j = 1; j <= p; j++) {
     b_size[j] = 0;
-#pragma omp task shared(b_size, b_size_t, num_t)
     for (uint32_t t = 0; t < num_t; t++)
-      b_size[j] += b_size_t[j][t];
+      b_size[j] += b_size_t[t][j - 1];
   }
-#pragma omp taskwait
-  for (uint32_t j = 1; j < p; j++)
+  for (uint32_t j = 1; j <= p; j++)
     b_size[j] += b_size[j - 1];
 
   uint32_t *b = new uint32_t[n];
-  for (uint32_t j = 0; j < n; j++) {
-    // #pragma omp task shared(data, b_id, b_size, b)
+  for (uint32_t t = 0; t < p; t++) {
+#pragma omp task shared(data, b_id, b_size, b)
     {
-      uint32_t id = b_id[j], idx;
-      // #pragma omp atomic capture
-      idx = b_size[id]++;
-      b[idx] = data[j];
+      for (uint64_t j = 0; j < n; j++) {
+        if (b_id[j] != t)
+          continue;
+        b[b_size[t]++] = data[j];
+      }
     }
   }
-  // #pragma omp taskwait
+#pragma omp taskwait
 
-  memcpy(data, b, n * sizeof(uint32_t));
-  delete[] b;
-  sz *= 2;
-  for (uint32_t j = 0; j < p; j++) {
-#pragma omp task shared(b_size, data, sz, p)
+  sz = 2 * (n / p);
+  for (uint64_t j = 0; j < p; j++) {
+#pragma omp task shared(b, b_size, data, sz, p)
     {
       uint32_t nj = (j == 0 ? b_size[j] : b_size[j] - b_size[j - 1]),
                i = b_size[j] - nj;
+      memcpy(data + i, b + i, nj * sizeof(uint32_t));
       if (nj < sz)
         SequentialSort(data + i, nj);
       else if (nj != 0)
@@ -125,4 +121,7 @@ void ParallelSort(uint32_t *data, uint32_t n, int p) {
     }
   }
 #pragma omp taskwait
+
+  delete[] b;
+  delete[] b_id;
 }
